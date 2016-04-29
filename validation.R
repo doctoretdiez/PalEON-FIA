a061e3505def012e85235602e3245bb9c9d7a441
Sean Dubois <sgdubois@russell.wisc.edu>
2016-04-29 10:28:01 -0500

    general updates to validation.R Add additional levels to FIA_biomass.R

--- validation.R
+++ validation.R
@@ -19,17 +19,18 @@ spptable <- read.csv("FIA_Rscript_imports/FIA_conversion-SGD_remove_dups.csv",he
 # class(spptable$acronym) <- "character"
 
 # Import Raster Files and Run validation
+scatcolor <- rgb(0,0,0,alpha=0.6)
 # ----------------------Basal area data-----------------------
-setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/BAS.STACK.MW.SG/')
+setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/BAS.STACK.SR/')
 ras_list <- list.files(pattern='*.tif')
 ras_files <- lapply(ras_list, raster)
-setwd('C:/Users/sgdubois/Dropbox/FIA_work/ValidationData/rasters-SG/basal_area/')
+setwd('C:/Users/sgdubois/Dropbox/FIA_work/ValidationData/albers/basal_area/')
 valid_ras_list <- list.files(pattern='*.tif')
 valid_ras_files <- lapply(valid_ras_list, raster)
 
 # Compare and Plot
 setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/Figures/')
-pdf('FIA_Simon_Validation_mw_basal_area_dynamic_axis.pdf', width=8.5, height=120)
+pdf('FIA_Sydne_Validation_v6_basal_area.pdf', width=8.5, height=120)
 par(mfrow=c(length(ras_list), 4))
 par(oma=c(0,0,2,0))
 nsims <- length(ras_list) #SGD ADDITION
@@ -52,7 +53,7 @@ for (i in 1:length(ras_list)){
     plot(comp.rast)
     myrange <- c(0,59)
     # plot(resamplefia, valid_file, xlim=myrange, ylim=myrange)
-    plot(resamplefia, valid_file)
+    plot(resamplefia, valid_file, col=scatcolor)
     abline(0,1,col="red",lwd=1,lty=3)
     #hist(comp.rast, main=NULL, xlab=NULL, ylab=NULL)
   } 
@@ -62,31 +63,32 @@ for (i in 1:length(ras_list)){
     frame()
     frame()
   }
-
+  
   setTxtProgressBar(pb, i)
 }
 mtext("FIA", side=3, line=0.5, adj=0.120, outer=TRUE)
-mtext("Simon", side=3, line=0.5, adj=0.370, outer=TRUE)
-mtext("FIA-Simon", side=3, line=0.5, adj=0.65, outer=TRUE)
-mtext("Simon vs. FIA", side=3, line=0.5, adj=0.93, outer=TRUE)
+mtext("Sydne", side=3, line=0.5, adj=0.370, outer=TRUE)
+mtext("FIA-Sydne", side=3, line=0.5, adj=0.65, outer=TRUE)
+mtext("Sydne vs. FIA", side=3, line=0.5, adj=0.93, outer=TRUE)
 
 dev.off()
 
 # ------------------------Biomass data---------------------------
-setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/BIO.STACK.MW.SG/')
+setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/BIO.STACK.SR/')
 ras_list <- list.files(pattern='*.tif')
 ras_files <- lapply(ras_list, raster)
-setwd('C:/Users/sgdubois/Dropbox/FIA_work/ValidationData/rasters-SG/biomass/')
+setwd('C:/Users/sgdubois/Dropbox/FIA_work/ValidationData/albers/biomass/')
 valid_ras_list <- list.files(pattern='*.tif')
 valid_ras_files <- lapply(valid_ras_list, raster)
 
 # Compare and Plot
 setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/Figures/')
-pdf('FIA_Simon_Validation_mw_biomass.pdf', width=8.5, height=120)
+pdf('FIA_Sydne_Validation_v6_biomass.pdf', width=8.5, height=120)
 par(mfrow=c(length(ras_list), 4))
 par(oma=c(0,0,2,0))
 nsims <- length(ras_list) #SGD ADDITION
 pb <- txtProgressBar(min = 1, max = nsims, style = 3) #SGD ADDITION
+lmlist <- list()
 for (i in 1:length(ras_list)){
   data_file <- GETDF_FROMLIST(ras_files, i)
   sppcode <- data.frame(strsplit(names(ras_files[[i]]), split = "_", fixed=TRUE))
@@ -94,53 +96,57 @@ for (i in 1:length(ras_list)){
   if (length(sppsciname)>1){stop(paste(sppcode[4,1], sppsciname[[1]], "Error, i=", i, sep=" "))}
   valid_data_number <- which(grepl(sppsciname[[1]], valid_ras_list))
   if (length(valid_data_number)>1){stop(paste(sppcode[4,1], sppsciname[[1]], "Error, i=", i, sep=" "))}
-#   if (length(valid_data_number)==1 && !(as.character(sppcode[4,1]) %in% c("BELE", "PIRI", "PIRU", "PIVI2", "QUIM", "QUPR2"))){
-  if (length(valid_data_number)==1){ 
-      valid_file <- GETDF_FROMLIST(valid_ras_files, valid_data_number)
+  if (length(valid_data_number)==1 && !(as.character(sppcode[4,1]) %in% c("BELE", "PIRI", "PIRU", "PIVI2", "QUIM", "QUPR2"))){
+  # if (length(valid_data_number)==1){ 
+    valid_file <- GETDF_FROMLIST(valid_ras_files, valid_data_number)/1000
     resamplefia <- data_file
     comp.rast <- resamplefia-valid_file
     #plot data
-    par(mar=c(2.1,3,1.6,2))
-    plot(resamplefia, main=sppcode[4,1])
-    plot(valid_file, main=sppsciname[[1]])
-    plot(comp.rast)
-    myrange <- c(0,350)
-    # plot(resamplefia, valid_file/1000, xlim=myrange, ylim=myrange)
-    plot(resamplefia, valid_file)
-    abline(0,1,col="red",lwd=1,lty=3)
+#     par(mar=c(2.1,3,1.6,2))
+#     plot(resamplefia, main=sppcode[4,1])
+#     plot(valid_file, main=sppsciname[[1]])
+#     plot(comp.rast)
+#     myrange <- c(0,350)
+#     # plot(resamplefia, valid_file/1000, xlim=myrange, ylim=myrange)
+#     plot(resamplefia, valid_file, col=scatcolor)
+#     abline(0,1,col="red",lwd=1,lty=3)
     #hist(comp.rast, main=NULL, xlab=NULL, ylab=NULL)
+    a <- lm(getValues(valid_file)~getValues(resamplefia)-1)
+    lmlist[i] <- a[1]
   } 
-  if (length(valid_data_number)!=1) {
-    plot(data_file, main=sppcode[4,1])
-    frame()
-    frame()
-    frame()
-  }
-#   if (as.character(sppcode[4,1]) %in% c("BELE", "PIRI", "PIRU", "PIVI2", "QUIM", "QUPR2")) {
+#   if (length(valid_data_number)!=1) {
 #     plot(data_file, main=sppcode[4,1])
-#     plot(valid_file/1000, main=sppsciname[[1]])
+#     frame()
 #     frame()
 #     frame()
 #   }
+#   if (as.character(sppcode[4,1]) %in% c("BELE", "PIRI", "PIRU", "PIVI2", "QUIM", "QUPR2")) {
+#     plot(data_file, main=sppcode[4,1])
+#     plot(valid_file, main=sppsciname[[1]])
+#     frame()
+#     frame()
+#   }
+
   setTxtProgressBar(pb, i)
 }
+AllOutput <- (do.call("rbind",lmlist))
 mtext("FIA", side=3, line=0.5, adj=0.120, outer=TRUE)
-mtext("Simon", side=3, line=0.5, adj=0.370, outer=TRUE)
-mtext("FIA-Simon", side=3, line=0.5, adj=0.65, outer=TRUE)
-mtext("Simon vs. FIA", side=3, line=0.5, adj=0.93, outer=TRUE)
+mtext("Sydne", side=3, line=0.5, adj=0.370, outer=TRUE)
+mtext("FIA-Sydne", side=3, line=0.5, adj=0.65, outer=TRUE)
+mtext("Sydne vs. FIA", side=3, line=0.5, adj=0.93, outer=TRUE)
 dev.off()
 
 # --------------------------DBH data---------------------------
-setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/DBH.STACK.MW.SG/')
+setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/DBH.STACK.SR/')
 ras_list <- list.files(pattern='*.tif')
 ras_files <- lapply(ras_list, raster)
-setwd('C:/Users/sgdubois/Dropbox/FIA_work/ValidationData/rasters-SG/diameter/')
+setwd('C:/Users/sgdubois/Dropbox/FIA_work/ValidationData/albers/diameter/')
 valid_ras_list <- list.files(pattern='*.tif')
 valid_ras_files <- lapply(valid_ras_list, raster)
 
 # Compare and Plot
 setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/Figures/')
-pdf('FIA_Simon_Validation_mw_dbh_dynamic_axis.pdf', width=8.5, height=120)
+pdf('FIA_Sydne_Validation_v6_dbh.pdf', width=8.5, height=120)
 par(mfrow=c(length(ras_list), 4))
 par(oma=c(0,0,2,0))
 nsims <- length(ras_list) #SGD ADDITION
@@ -152,8 +158,8 @@ for (i in 1:length(ras_list)){
   if (length(sppsciname)>1){stop(paste(sppcode[4,1], sppsciname[[1]], "Error, i=", i, sep=" "))}
   valid_data_number <- which(grepl(sppsciname[[1]], valid_ras_list))
   if (length(valid_data_number)>1){stop(paste(sppcode[4,1], sppsciname[[1]], "Error, i=", i, sep=" "))}
-  # if (length(valid_data_number)==1 && !(as.character(sppcode[4,1]) %in% c("PIRI", "PIVI2", "QUPR2"))){
-  if (length(valid_data_number)==1){ 
+  if (length(valid_data_number)==1 && !(as.character(sppcode[4,1]) %in% c("PIRI", "PIVI2", "QUPR2"))){
+  # if (length(valid_data_number)==1){ 
     valid_file <- GETDF_FROMLIST(valid_ras_files, valid_data_number)
     resamplefia <- data_file
     comp.rast <- resamplefia-valid_file
@@ -164,7 +170,7 @@ for (i in 1:length(ras_list)){
     plot(comp.rast)
     myrange <- c(20,140) #DBH
     # plot(resamplefia, valid_file, xlim=myrange, ylim=myrange)
-    plot(resamplefia, valid_file)
+    plot(resamplefia, valid_file, col=scatcolor)
     abline(0,1,col="red",lwd=1,lty=3)
     #hist(comp.rast, main=NULL, xlab=NULL, ylab=NULL)
   } 
@@ -174,35 +180,36 @@ for (i in 1:length(ras_list)){
     frame()
     frame()
   }
-#   if (as.character(sppcode[4,1]) %in% c("PIRI", "PIVI2", "QUPR2")) {
-#     plot(data_file, main=sppcode[4,1])
-#     plot(valid_file, main=sppsciname[[1]])
-#     frame()
-#     frame()
-#   }
+  if (as.character(sppcode[4,1]) %in% c("PIRI", "PIVI2", "QUPR2")) {
+    plot(data_file, main=sppcode[4,1])
+    plot(valid_file, main=sppsciname[[1]])
+    frame()
+    frame()
+  }
   setTxtProgressBar(pb, i)
 }
 mtext("FIA", side=3, line=0.5, adj=0.120, outer=TRUE)
-mtext("Simon", side=3, line=0.5, adj=0.370, outer=TRUE)
-mtext("FIA-Simon", side=3, line=0.5, adj=0.65, outer=TRUE)
-mtext("Simon vs. FIA", side=3, line=0.5, adj=0.93, outer=TRUE)
+mtext("Sydne", side=3, line=0.5, adj=0.370, outer=TRUE)
+mtext("FIA-Sydne", side=3, line=0.5, adj=0.65, outer=TRUE)
+mtext("Sydne vs. FIA", side=3, line=0.5, adj=0.93, outer=TRUE)
 dev.off()
 
 # ------------------------Density data---------------------------
-setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/DEN.STACK.MW.SG/')
+setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/DEN.STACK.SR/')
 ras_list <- list.files(pattern='*.tif')
 ras_files <- lapply(ras_list, raster)
-setwd('C:/Users/sgdubois/Dropbox/FIA_work/ValidationData/rasters-SG/density/')
+setwd('C:/Users/sgdubois/Dropbox/FIA_work/ValidationData/albers/density/')
 valid_ras_list <- list.files(pattern='*.tif')
 valid_ras_files <- lapply(valid_ras_list, raster)
 
 # Compare and Plot
 setwd('C:/Users/sgdubois/Dropbox/FIA_work/CodeOutput/Figures/')
-pdf('FIA_Simon_Validation_mw_density_dynamic_axis.pdf', width=8.5, height=120)
+pdf('FIA_Sydne_Validation_v6_density.pdf', width=8.5, height=120)
 par(mfrow=c(length(ras_list), 4))
 par(oma=c(0,0,2,0))
 nsims <- length(ras_list) #SGD ADDITION
 pb <- txtProgressBar(min = 1, max = nsims, style = 3) #SGD ADDITION
+lmlist <- list()
 for (i in 1:length(ras_list)){
   data_file <- GETDF_FROMLIST(ras_files, i)
   sppcode <- data.frame(strsplit(names(ras_files[[i]]), split = "_", fixed=TRUE))
@@ -215,26 +222,30 @@ for (i in 1:length(ras_list)){
     resamplefia <- data_file
     comp.rast <- resamplefia-valid_file
     #plot data
-    par(mar=c(2.1,3,1.6,2))
-    plot(resamplefia, main=sppcode[4,1])
-    plot(valid_file, main=sppsciname[[1]])
-    plot(comp.rast)
-    myrange <- c(0,700) #Density
-    # plot(resamplefia, valid_file, xlim=myrange, ylim=myrange)
-    plot(resamplefia, valid_file)
-    abline(0,1,col="red",lwd=1,lty=3)
+#     par(mar=c(2.1,3,1.6,2))
+#     plot(resamplefia, main=sppcode[4,1])
+#     plot(valid_file, main=sppsciname[[1]])
+#     plot(comp.rast)
+#     myrange <- c(0,700) #Density
+#     # plot(resamplefia, valid_file, xlim=myrange, ylim=myrange)
+#     plot(resamplefia, valid_file, col=scatcolor)
+#     abline(0,1,col="red",lwd=1,lty=3)
     #hist(comp.rast, main=NULL, xlab=NULL, ylab=NULL)
+    a <- lm(getValues(valid_file)~getValues(resamplefia)-1)
+    lmlist[i] <- a[1]
   } 
-  if (length(valid_data_number)!=1) {
-    plot(data_file, main=sppcode[4,1])
-    frame()
-    frame()
-    frame()
-  }
+#   if (length(valid_data_number)!=1) {
+#     plot(data_file, main=sppcode[4,1])
+#     frame()
+#     frame()
+#     frame()
+#   }
   setTxtProgressBar(pb, i)
 }
+AllOutput <- (do.call("rbind",lmlist))
+
 mtext("FIA", side=3, line=0.5, adj=0.120, outer=TRUE)
-mtext("Simon", side=3, line=0.5, adj=0.370, outer=TRUE)
-mtext("FIA-Simon", side=3, line=0.5, adj=0.65, outer=TRUE)
-mtext("Simon vs. FIA", side=3, line=0.5, adj=0.93, outer=TRUE)
+mtext("Sydne", side=3, line=0.5, adj=0.370, outer=TRUE)
+mtext("FIA-Sydne", side=3, line=0.5, adj=0.65, outer=TRUE)
+mtext("Sydne vs. FIA", side=3, line=0.5, adj=0.93, outer=TRUE)
 dev.off()
