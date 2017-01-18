d3642b5d689dd70f11c32db4bd146e30e8d707f9
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2017-01-17 18:35:26 -0800

    reworking processing to work with revised input data files on wiki

--- stats/preprocess_biomass.R
+++ stats/preprocess_biomass.R
@@ -2,15 +2,38 @@ library(readr)
 library(magrittr)
 library(dplyr)
 
+# download files from Wiki:
+# biom_fia_pecan.zip (unzip this)
+# fia_paleongrid_albers.csv
+# fia_conversion_v02-sgd.csv
+
+# decide if use Jenkins or Pecan allometry
+# for now if use Pecan, just use point estimate, namely biomass_pecan_pred_mean
+# CJP believes that 'pred' are the columns to use as these should incorporate tree-to-tree variability
+
+dataDir <- '.'
+
+dataFile <- 'biom_fia_pecan.csv'
+gridFile <- 'fia_paleongrid_albers.csv'
+taxaFile <- 'fia_conversion_v02-sgd.csv'
+
+biomassCol <- 'biomass_pecan_pred_mean'
+
 # using character string here for safety because FIA plot ids are near the maximum length of integers in 16-bit numeric representation
-setwd(file.path('..','Conversion_tables'))
-grid <- read_csv('FIA_paleongrid_albers.csv', col_types = 'dcddd') %>%
+
+setwd(dataDir)
+
+grid <- read_csv(gridFile, col_types = 'dcddd') %>%
     filter(STATECD %in% c(26, 27, 55))  # upper midwest
-taxa <- read_csv('FIA_conversion-SGD_remove_dups.csv')
-# cp ~/Downloads/tree_data5.csv ../data
-data <- read_csv('~/Downloads/tree_data5.csv',
-                 col_types = 'cdcdddddcddddddddddcd') %>%
-    select(plt_cn, PalEON, time, Jenkins_Biomass)
+taxa <- read_csv(taxaFile)
+data <- read_csv(dataFile,
+                 col_types = 'cdcdddddcddddddddddcd')
+# next lines are gymnastics so can treat the biomass column programmatically within dplyr syntax
+nm <- names(data)
+wh <- which(nm == biomassCol)
+nm[wh] <- 'biomass_use'
+names(data) <- nm
+data <- data %>% select(plt_cn, PalEON, time, biomass_use)
 
 
 albers <- grid %>% select(x,y,cell) %>% unique()
