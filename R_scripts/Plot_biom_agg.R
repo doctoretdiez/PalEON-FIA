30835b4251797500054a940e5931096ac9f756cb
SimonGoring <simon.j.goring@gmail.com>
2017-03-27 15:52:37 -0700

    Added `plyr` and `raster` functionality to the `control_file.Rmd`.
    Also adds the `agg_to_plot` function into the `Rmd`, which is a result of pulling out the `Plot_biom_agg.R` code and putting it into the `agg_to_plot` function.
    
    The requirement for `plyr` is a result of some changes in the `Plot_parameters_agg.R` file.  Did some `lintr`ing as well to the same file.
    
    I think that the `Rasterize_biom.R` file needs to be turned into a function, but haven't done that yet.

--- R_scripts/Plot_biom_agg.R
+++ R_scripts/Plot_biom_agg.R
@@ -2,29 +2,18 @@
 # Adapted from FIA_biomass.R
 # Created by SGD 06/07/16
 
-# Load packages
-if(!require(plyr)){
-  install.packages('plyr')
-  require(plyr)
-} else {require(plyr)}
+# Modified by SJG - Converted to function, plus added dplyr & tidyr methods to speed up
+# aggregation & spreading.
 
-
-# Load biomass estimates
-
-Jenkins_biom <- read.csv('data/output/biom_fia_jenk.csv')
-FIA_biom <- read.csv('data/output/biom_fia_fia.csv')
-PEcAn_biom <- read.csv('data/output/biom_fia_pecan.csv')
-
-# --------------Calculate plot level biomass estimates--------
-species_FIA_bio <- aggregate(FIA_total_biomass ~ spcd + plt_cn, FIA_biom, FUN=sum)
-species_Jenkins_bio <- aggregate(Jenkins_Biomass ~ spcd + plt_cn, Jenkins_biom, FUN=sum)
-species_PEcAn_bio <- aggregate(PEcAn_Biomass ~ spcd + plt_cn, PEcAn_biom, FUN=sum)
-
-# Compare Jenkins and FIA plot level estimates
-plot(species_Jenkins_bio$Jenkins_Biomass, species_FIA_bio$FIA_total_biomass)
-abline(0,1,col="red")
-
-# Combine all
-species_plot_bio <- join_all(list(species_FIA_bio, species_Jenkins_bio, species_PEcAn_bio), type = "full")
-
-write.csv(species_plot_bio, 'data/output/species_plot_biom.csv', row.names=FALSE)
+agg_to_plot <- function(tree_data, column, write_out = TRUE) {
+  
+  long_plot <- tree_data %>% dplyr::group_by(plt_cn, spcd) %>% dplyr::summarise(paste0("biomass = sum(",column,")"))
+  
+  colnames(long_plot)[3] <- "biomass"
+  
+  wide_plot <- long_plot %>% tidyr::spread(spcd, biomass) %>% as.data.frame
+  
+  if (write_out == TRUE) write.csv(wide_plot, paste0('data/output/',column, '.csv'))
+  
+  wide_plot
+}
--- R_scripts/Plot_parameters_agg.R
+++ R_scripts/Plot_parameters_agg.R
@@ -4,35 +4,24 @@
 
 options(scipen = 999)
 
-# Load packages
-if(!require(plyr)){
-  install.packages('plyr')
-  require(plyr)
-} else {require(plyr)}
-
-if(!require(dplyr)){
-  install.packages('dplyr')
-  require(dplyr)
-} else {require(dplyr)}
-
 # Constants
 tpa <- 6.018046
 ac2ha   <- 0.404686   #conversion from 1 acre to hectares
 
 # Read in tree data
-surv.current <- read.csv('data/output/full_fia_long.csv', header=TRUE)
+surv.current <- read.csv('data/output/full_fia_long.csv', header = TRUE)
 
 # ----------Estimate FIA parameters at the species level-------------------
 # Mean dbh
-species_FIA_dbh <- aggregate(dbh ~ spcd + plt_cn, surv.current, FUN=mean)
+species_FIA_dbh <- aggregate(dbh ~ spcd + plt_cn, surv.current, FUN = mean)
 # Species count
 tree_count <- surv.current %>% dplyr::count(plt_cn, spcd)
 # Species density
 tree_count$density <- tree_count$n * tpa* (1/ac2ha)
 # Basal area
-surv.current$basal_area <- (surv.current$dbh/2/100)^2 * pi * tpa * (1/ac2ha) # units m^2/ha
-species_FIA_basalarea <- aggregate(basal_area ~ spcd + plt_cn, surv.current, FUN=sum)
+surv.current$basal_area <- (surv.current$dbh/2/100) ^ 2 * pi * tpa * (1/ac2ha) # units m^2/ha
+species_FIA_basalarea <- aggregate(basal_area ~ spcd + plt_cn, surv.current, FUN = sum)
 
 species_plot_parameters <- join_all(list(species_FIA_dbh, species_FIA_basalarea, tree_count), type = "full")
 
-write.csv(species_plot_parameters, 'data/output/species_plot_parameters.csv', row.names=FALSE)
+write.csv(species_plot_parameters, 'data/output/species_plot_parameters.csv', row.names = FALSE)
